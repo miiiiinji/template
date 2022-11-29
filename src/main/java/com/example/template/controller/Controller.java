44285c3e8d862e02a06f44b65789539a0502e151
miiiiinji <wnbnjust@gmail.com>
2022-11-29 13:56:19 +0000

    ✨ [Feat]: add getMapping with parameter Added getMappng function with parameter

--- src/main/java/com/example/template/controller/Controller.java
+++ src/main/java/com/example/template/controller/Controller.java
@@ -1,13 +1,31 @@
 package com.example.template.controller;
 
+import com.example.template.service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    private final Service service;
+
+    @Autowired
+    public Controller(Service service){
+        this.service = service;
+    }
+
     @GetMapping(value="/test")
     public String test(){
         return "rest api test2";
     }
+
+    @GetMapping(value="/service/{number}")
+    public int getService(@PathVariable int number) {
+
+        int result = service.serviceTemplate(number);
+        return result;
+    }
+
 }
--- src/main/java/com/example/template/service/Service.java
+++ src/main/java/com/example/template/service/Service.java
@@ -1,4 +1,10 @@
 package com.example.template.service;
 
+@org.springframework.stereotype.Service
 public class Service {
+
+    public int serviceTemplate(int a){
+        return a*2;
+    }
+
 }
